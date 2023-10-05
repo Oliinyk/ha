42472de098f60c5881ebd1eae29fdd50faa95976
Roman <oliinykroman@gmail.com>
2023-10-05 13:09:27 +0300

    home page -v

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,22 +8,22 @@
     "use strict";
 
 
-    var flatAccordions = function() {
-        var args = { easing: 'easeOutExpo', duration: 500 };
+    // var flatAccordions = function() {
+    //     var args = { easing: 'easeOutExpo', duration: 500 };
 
-        $('.accordion-item.active').find('.accordion-content').show();
-        $('.accordion-heading').on('click', function() {
-            if (!$(this).parent().is('.active')) {
-                $(this).parent().toggleClass('active')
-                    .children('.accordion-content').slideToggle(args)
-                    .parent().siblings('.active').removeClass('active')
-                    .children('.accordion-content').slideToggle(args);
-            } else {
-                $(this).parent().toggleClass('active');
-                $(this).next().slideToggle(args);
-            }
-        });
-    };
+    //     $('.accordion-item.active').find('.accordion-content').show();
+    //     $('.accordion-heading').on('click', function() {
+    //         if (!$(this).parent().is('.active')) {
+    //             $(this).parent().toggleClass('active')
+    //                 .children('.accordion-content').slideToggle(args)
+    //                 .parent().siblings('.active').removeClass('active')
+    //                 .children('.accordion-content').slideToggle(args);
+    //         } else {
+    //             $(this).parent().toggleClass('active');
+    //             $(this).next().slideToggle(args);
+    //         }
+    //     });
+    // };
 
 
     // PreLoad
@@ -36,10 +36,23 @@
         });
     };
 
+
+    //
+    let Tab = function() {
+        $(".menu-tab li").click(function() {
+            $(".menu-tab li").removeClass("active").eq($(this).index()).addClass("active");
+            $(".content-tab .content-item").hide().eq($(this).index()).fadeIn()
+        }).eq(0).addClass("active");
+    };
+
+
+
+
     // Dom Ready
     $(function() {
-        flatAccordions();
+        // flatAccordions();
         Preloader();
+        Tab();
     });
 
 })(jQuery);
